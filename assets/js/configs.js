288a7a68fb7e89af2fe271f1264a9d4d0e89def3
Geraldo Neto <geraldo.linkedit@gmail.com>
2026-01-16 14:18:53 -0300

    projeto-v01

---
+++ assets/js/configs.js
@@ -0,0 +1,27 @@
+// assets/js/configs.js
+export const msoConfigs = {
+  // Animação isolada (não-MSO): #item71793
+  item71793: {
+    default: {
+      containerId: 'item71793',
+      path: './assets/json/capa.json',
+      loop: true // SOMENTE esta em loop
+    }
+  },
+
+  item74487: {
+    'instituicao': {
+      containerId: 'item74279',
+      path: './assets/json/financeira.json'
+    },
+    'grupo': {
+      containerId: 'item74269',
+      path: './assets/json/grupo-empresarial.json'
+    },
+    'cooperativa': {
+      containerId: 'item74261',
+      path: './assets/json/cooperativa.json'
+    }
+  },
+
+};
