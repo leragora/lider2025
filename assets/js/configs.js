0669e3c1d2788d138ff79dfed98264e585a0f997
Geraldo Neto <geraldo.linkedit@gmail.com>
2026-02-10 17:35:19 -0300

    rev01

--- assets/js/configs.js
+++ assets/js/configs.js
@@ -10,18 +10,199 @@ export const msoConfigs = {
   },
 
   item74487: {
+    'construcao': {
+      containerId: 'item74261',
+      path: './assets/json/contrucao.json'
+    }
+  },
+
+  item77749: {
+    'logistica': {
+      containerId: 'item77802',
+      path: './assets/json/logistica.json'
+    }
+  },
+
+  item79380: {
+    'engenharia': {
+      containerId: 'item79433',
+      path: './assets/json/engenharia.json'
+    }
+  },
+
+  item77324: {
     'instituicao': {
-      containerId: 'item74279',
+      containerId: 'item77377',
       path: './assets/json/financeira.json'
     },
-    'grupo': {
-      containerId: 'item74269',
+       'grupo': {
+      containerId: 'item77379',
       path: './assets/json/grupo-empresarial.json'
     },
-    'cooperativa': {
-      containerId: 'item74261',
+       'cooperativa': {
+      containerId: 'item77381',
       path: './assets/json/cooperativa.json'
     }
   },
+
+  item80336: {
+    'ferragens': {
+      containerId: 'item80390',
+      path: './assets/json/ferragens.json'
+    }
+  },
+
+  item80693: {
+    'alimentos': {
+      containerId: 'item80746',
+      path: './assets/json/alimentos-bebidas.json'
+    }
+  },
+
+  item90816: {
+    'plano-saude': {
+      containerId: 'item90869',
+      path: './assets/json/plano-saude.json'
+    }
+  },
+
+  item81125: {
+    'eletricidade': {
+      containerId: 'item81127',
+      path: './assets/json/eletricidade.json'
+    }
+  },
+
+  item82474: {
+    'esg': {
+      containerId: 'item82527',
+      path: './assets/json/esg.json'
+    }
+  },
+
+  item82833: {
+    'automotivo': {
+      containerId: 'item82887',
+      path: './assets/json/automotivo.json'
+    }
+  },
+
+  item83252: {
+    'proteina': {
+      containerId: 'item83306',
+      path: './assets/json/proteina.json'
+    }
+  },
+
+  item83622: {
+    'construcao': {
+      containerId: 'item83675',
+      path: './assets/json/construcao.json'
+    }
+  },
+
+  item84476: {
+    'escola-privada': {
+      containerId: 'item84530',
+      path: './assets/json/escola-privada.json'
+    }
+  },
+
+  item84888: {
+    'maior': {
+      containerId: 'item84941',
+      path: './assets/json/maiores.json'
+    },
+       'supermercado': {
+      containerId: 'item84943',
+      path: './assets/json/supermercado.json'
+    }
+  },
+
+  item85456: {
+    'moveleira': {
+      containerId: 'item85509',
+      path: './assets/json/moveleira.json'
+    }
+  },
+
+  item85805: {
+    'advocacia': {
+      containerId: 'item85859',
+      path: './assets/json/advocacia.json'
+    }
+  },
+
+  item86175: {
+    'loteadora': {
+      containerId: 'item86228',
+      path: './assets/json/loteadora.json'
+    }
+  },
+
+  item86537: {
+    'hospital-privado': {
+      containerId: 'item86590',
+      path: './assets/json/hospital-privado.json'
+    }
+  },
+
+  item87721: {
+    'federacao': {
+      containerId: 'item87774',
+      path: './assets/json/federacao.json'
+    }
+  },
+
+  item87283: {
+    'financeira': {
+      containerId: 'item87338',
+      path: './assets/json/investimentos.json'
+    }
+  },
+
+  item91230: {
+    'shopping': {
+      containerId: 'item91283',
+      path: './assets/json/shopping.json'
+    }
+  },
+
+  item86902: {
+    'farmacia': {
+      containerId: 'item86955',
+      path: './assets/json/farmacia.json'
+    }
+  },
+
+  item88092: {
+    'atacadista': {
+      containerId: 'item88145',
+      path: './assets/json/atacadista.json'
+    }
+  },
+
+  item88498: {
+    'cafe': {
+      containerId: 'item88552',
+      path: './assets/json/cafe.json'
+    }
+  },
+
+  item88945: {
+    'superior': {
+      containerId: 'item88998',
+      path: './assets/json/superior.json'
+    }
+  },
+
+  item89331: {
+    'ambiental': {
+      containerId: 'item89384',
+      path: './assets/json/ambiental.json'
+    }
+  },
+
+
 
 };
